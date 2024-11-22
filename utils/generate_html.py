3c38830a8d2188f21ed7f4096034df8dbc31918c
dippingsauce <jcucuzza2000@gmail.com>
2024-11-22 00:31:04 -0600

    fix src again

--- utils/generate_html.py
+++ utils/generate_html.py
@@ -42,7 +42,7 @@ def generate_recipes(recipes: list[recipe_model.Recipe]):
                             <figure class="image is-4by3">
                             <img
                                 style="object-fit: cover"
-                                src="/public/static/img/{slug}.jpg"
+                                src="/static/img/{slug}.jpg"
                                 alt="Placeholder image"
                             />
                             </figure>
